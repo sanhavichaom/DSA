02d3fe372a93aebec328a2116b89f4066d37cc3a
sanhavichaom <122725863+sanhavichaom@users.noreply.github.com>
2026-02-15 01:10:59 +0700

    Time: 32 ms (19.91%), Space: 44.8 MB (10.23%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cs
@@ -0,0 +1,19 @@
+public class Solution {
+    public bool IsAnagram(string s, string t) {
+        char[] str1 = s.ToCharArray();
+        char[] str2 = t.ToCharArray();
+
+        if(str1.Length != str2.Length){
+            return false;
+        }
+
+        Array.Sort(str1);
+        Array.Sort(str2);
+        for(int i = 0; i < str1.Length; i++){
+            if(str1[i] != str2[i]){
+                return false;
+            }
+        }
+        return true;
+    }
+}
