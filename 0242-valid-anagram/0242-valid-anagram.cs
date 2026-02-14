b9a271f893dd9efae80e4321acc880f06367335e
sanhavichaom <122725863+sanhavichaom@users.noreply.github.com>
2026-02-15 03:03:01 +0700

    Time: 5 ms (64.44%), Space: 43.6 MB (87.25%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cs
+++ 0242-valid-anagram/0242-valid-anagram.cs
@@ -1,52 +1,43 @@
 public class Solution {
     public bool IsAnagram(string s, string t) {
-        var map1 = new Dictionary<char, int>();
-        var map2 = new Dictionary<char, int>();
-
         if(s.Length != t.Length){
             return false;
         }
-        
-        for (int i = 0; i < s.Length; i++)
-        {
-            char c = s[i]; 
 
-   
-            if (!map1.ContainsKey(c))
+        var map1 = new Dictionary<char, int>();
+        var map2 = new Dictionary<char, int>();
+
+        foreach(char c in s){
+            if(!map1.ContainsKey(c)){
                 map1[c] = 0;
-
-
+            }
             map1[c]++;
         }
 
-        for (int i = 0; i < t.Length; i++)
-        {
-            char c = t[i];
-
-            if (!map2.ContainsKey(c))
+        foreach(char c in t){
+            if(!map2.ContainsKey(c)){
                 map2[c] = 0;
-
+            }
             map2[c]++;
         }
 
-        if (map1.Count != map2.Count)
+        if(map1.Count != map2.Count){
             return false;
+        }
 
-        foreach (var pair in map1)
-        {
-            char key = pair.Key;     
-            int value = pair.Value;  
+        foreach(var pair in map1){
+            char key = pair.Key;
+            int value = pair.Value;
 
-
-            if (!map2.ContainsKey(key))
+            if(!map2.ContainsKey(key)){
                 return false;
+            }
 
-
-            if (map2[key] != value)
+            if(map2[key] != value){
                 return false;
+            }
         }
 
         return true;
-
     }
 }
