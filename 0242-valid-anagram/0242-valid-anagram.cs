117632e6a95dcc6d3ec3336a9acdb581a68e81d2
sanhavichaom <122725863+sanhavichaom@users.noreply.github.com>
2026-02-15 01:37:59 +0700

    Time: 37 ms (10.72%), Space: 44.3 MB (22.44%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cs
+++ 0242-valid-anagram/0242-valid-anagram.cs
@@ -6,9 +6,10 @@ public class Solution {
         if(str1.Length != str2.Length){
             return false;
         }
-
+        
         Array.Sort(str1);
         Array.Sort(str2);
+
         for(int i = 0; i < str1.Length; i++){
             if(str1[i] != str2[i]){
                 return false;
